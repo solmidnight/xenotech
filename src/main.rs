c40acf58aebd052e5f7fa968afce5d8db137f482
Sol Midnight <the.sol.midnight@gmail.com>
2023-12-14 15:53:25 -0600

    Thruster allocation almost working\!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::{iter, f32::{consts::PI, EPSILON}};
+use std::{
+    f32::{consts::PI, EPSILON},
+    iter,
+};
 
 use bevy::{
     prelude::*,
     render::{mesh::Indices, render_resource::PrimitiveTopology},
-    utils::{hashbrown::HashSet, HashMap}, sprite::{Mesh2dHandle, MaterialMesh2dBundle},
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+    utils::{hashbrown::HashSet, HashMap},
 };
-use bevy_xpbd_2d::{prelude::*, parry::na::ComplexField};
+use bevy_xpbd_2d::{parry::na::ComplexField, prelude::*};
 use bitflags::bitflags;
 use itertools::Itertools;
 
 bitflags! {
-    #[derive(PartialEq, Eq, Hash, Clone, Copy, Default)]
+    #[derive(PartialEq, Eq, Hash, Clone, Copy, Default, Debug)]
     pub struct Direction: u32 {
         const LEFT = 0b00000001;
         const RIGHT = 0b00000010;
@@ -55,7 +59,7 @@ impl Cell {
     }
 }
 
-#[derive(Component)]
+#[derive(Component, Default)]
 pub struct Object {
     data: HashMap<IVec2, Cell>,
     minimum: IVec2,
@@ -63,6 +67,28 @@ pub struct Object {
 }
 
 impl Object {
+    fn calc_bounds(&mut self) {
+        let mut minimum = IVec2::MAX;
+        let mut inc_maximum = IVec2::MIN;
+
+        for (pos, _) in &self.data {
+            if minimum.x > pos.x {
+                minimum.x = pos.x;
+            }
+            if minimum.y > pos.y {
+                minimum.y = pos.y;
+            }
+            if inc_maximum.x < pos.x {
+                inc_maximum.x = pos.x;
+            }
+            if inc_maximum.y < pos.y {
+                inc_maximum.y = pos.y;
+            }
+        }
+
+        self.minimum = minimum;
+        self.maximum = inc_maximum + 1;
+    }
     fn size(&self) -> UVec2 {
         (self.maximum - self.minimum).as_uvec2()
     }
@@ -78,20 +104,20 @@ fn create_object_mesh(object: &Object) -> MeshData {
     loop {
         let mut size = IVec2::splat(0);
 
-        for x in object.minimum.x..object.maximum.x {
+        'a: for x in object.minimum.x..object.maximum.x {
             for y in object.minimum.y..object.maximum.y {
                 if !visited.contains(&IVec2 { x, y }) && object.data.contains_key(&IVec2 { x, y }) {
                     cursor = IVec2 { x, y };
+                    break 'a;
                 }
             }
         }
-        
+        dbg!(&visited, object.minimum, object.maximum);
 
         let template = object.data[&cursor].clone();
         while object.data.contains_key(&(cursor + size))
             && object.data[&(cursor + size)] == template
         {
-            visited.insert(cursor + size);
             size.x += 1;
         }
         size.y += 1;
@@ -103,18 +129,21 @@ fn create_object_mesh(object: &Object) -> MeshData {
                     && object.data[&(cursor + offset)] == template
             }) {
                 size.y += 1;
-                for offset in range {
-                    visited.insert(cursor + offset);
-                }
             } else {
                 break;
             }
         }
 
-            groups.entry(template).or_default().push((cursor, size));
-        cursor = cursor + size;
+        for x in cursor.x..cursor.x + size.x {
+            for y in cursor.y..cursor.y + size.y {
+                if object.data.contains_key(&IVec2 { x, y }) {
+                    visited.insert(IVec2 { x, y });
+                }
+            }
+        }
 
-        
+        groups.entry(template).or_default().push((cursor, size));
+
         if visited.len() == object.data.len() {
             break;
         }
@@ -135,7 +164,6 @@ fn create_object_mesh(object: &Object) -> MeshData {
 
     for (cell, pairs) in groups {
         for (pos, size) in pairs {
-
             let count = vertices.len();
             vertices.extend(TEMPLATE_VERTICES.iter().copied().map(|[mut x, mut y, z]| {
                 x = x * size.x as f32 + pos.x as f32;
@@ -144,10 +172,14 @@ fn create_object_mesh(object: &Object) -> MeshData {
             }));
             indices.extend(TEMPLATE_INDICES.iter().copied().map(|i| i + count as u32));
             colors.extend(iter::repeat(cell.color()).take(TEMPLATE_VERTICES.len()));
-        
-        }}
+        }
+    }
 
-    MeshData { vertices, indices, colors }
+    MeshData {
+        vertices,
+        indices,
+        colors,
+    }
 }
 
 pub struct MeshData {
@@ -163,10 +195,16 @@ fn setup(
 ) {
     let mut camera_transform = Transform::from_xyz(0.0, 0.0, 100.0);
     camera_transform.look_at(Vec3::ZERO, Vec3::Y);
-    camera_transform.rotate_z(PI);
     let camera_id = commands
-        .spawn(Camera2dBundle { transform: camera_transform, projection: OrthographicProjection { scale: 0.2, ..default()}, ..default() }).id();
-    
+        .spawn(Camera2dBundle {
+            transform: camera_transform,
+            projection: OrthographicProjection {
+                scale: 0.2,
+                ..default()
+            },
+            ..default()
+        })
+        .id();
 
     let mut data = HashMap::new();
     data.insert(IVec2::new(0, 1), Cell::Metal);
@@ -178,14 +216,21 @@ fn setup(
     data.insert(IVec2::new(2, 2), Cell::Thruster(Direction::DOWN));
     data.insert(IVec2::new(-1, 1), Cell::Thruster(Direction::LEFT));
     data.insert(IVec2::new(3, 1), Cell::Thruster(Direction::RIGHT));
-    let object = Object { data, minimum: IVec2::new(-1, 0), maximum: IVec2::new(4, 3) };
+    let mut object = Object { data, ..default() };
+    object.calc_bounds();
 
-    let MeshData { vertices, indices, colors } = create_object_mesh(&object);
+    let MeshData {
+        vertices,
+        indices,
+        colors,
+    } = create_object_mesh(&object);
 
-    let mesh = meshes.add(Mesh::new(PrimitiveTopology::TriangleList)
-    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices.clone())
-    .with_inserted_attribute(Mesh::ATTRIBUTE_COLOR, colors)
-    .with_indices(Some(Indices::U32(indices.clone()))));
+    let mesh = meshes.add(
+        Mesh::new(PrimitiveTopology::TriangleList)
+            .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices.clone())
+            .with_inserted_attribute(Mesh::ATTRIBUTE_COLOR, colors)
+            .with_indices(Some(Indices::U32(indices.clone()))),
+    );
 
     let material = materials.add(ColorMaterial {
         color: Color::Rgba {
@@ -196,23 +241,34 @@ fn setup(
         },
         ..default()
     });
-    let collider = Collider::trimesh(vertices.into_iter().map(|[x, y, _]| Vec2 { x, y} ).collect::<Vec<_>>(), indices.chunks(3).map(|chunk| [chunk[0], chunk[1], chunk[2]]).collect::<Vec<_>>());
-    commands.spawn((
-        Input::default(),
-        RigidBody::Dynamic,
-        ExternalForce::default(),
-        ExternalTorque::default(),
-        LinearVelocity::default(),
-        AngularVelocity::default(),
-        object,
-        collider.clone(),
-        MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(mesh.clone()),
-            material: material.clone(),
-            transform: Transform::from_xyz(10.0, 0.0, 0.0),
-            ..default()
-        },
-    )).add_child(camera_id);
+    let collider = Collider::trimesh(
+        vertices
+            .into_iter()
+            .map(|[x, y, _]| Vec2 { x, y })
+            .collect::<Vec<_>>(),
+        indices
+            .chunks(3)
+            .map(|chunk| [chunk[0], chunk[1], chunk[2]])
+            .collect::<Vec<_>>(),
+    );
+    commands
+        .spawn((
+            Input::default(),
+            RigidBody::Dynamic,
+            ExternalForce::default(),
+            ExternalTorque::default(),
+            LinearVelocity::default(),
+            AngularVelocity::default(),
+            object,
+            collider.clone(),
+            MaterialMesh2dBundle {
+                mesh: Mesh2dHandle(mesh.clone()),
+                material: material.clone(),
+                transform: Transform::from_xyz(10.0, 0.0, 0.0),
+                ..default()
+            },
+        ))
+        .add_child(camera_id);
     commands.spawn((
         RigidBody::Dynamic,
         collider,
@@ -231,7 +287,11 @@ pub struct Input {
     rotate: f32,
 }
 
-fn input(query1: Query<(&Parent, &Camera)>, mut query2: Query<&mut Input>, keys: Res<bevy::prelude::Input<KeyCode>>,) {
+fn input(
+    query1: Query<(&Parent, &Camera)>,
+    mut query2: Query<&mut Input>,
+    keys: Res<bevy::prelude::Input<KeyCode>>,
+) {
     let _ = keys;
     let (p, _) = query1.single();
 
@@ -255,13 +315,20 @@ fn input(query1: Query<(&Parent, &Camera)>, mut query2: Query<&mut Input>, keys:
     i.rotate = keys.pressed(KeyCode::Q) as i32 as f32 - keys.pressed(KeyCode::E) as i32 as f32;
 }
 
-fn thruster_alloc(mut query: Query<(&Object, &Input, &Transform, &CenterOfMass, &mut LinearVelocity, &mut AngularVelocity, &mut ExternalForce, &mut ExternalTorque)>) {
+fn thruster_alloc(
+    mut query: Query<(
+        &Object,
+        &Input,
+        &Transform,
+        &CenterOfMass,
+        &mut LinearVelocity,
+        &mut AngularVelocity,
+        &mut ExternalForce,
+        &mut ExternalTorque,
+    )>,
+) {
     for (o, i, t, com, mut v, mut a, mut ef, mut et) in query.iter_mut() {
-        let rn = t.rotation.to_euler(EulerRot::ZYX);
-        let mut r = Mat2::default();
-        r.x_axis = Vec2::new(rn.0.cos(), rn.0.sin());
-        r.y_axis = Vec2::new(-rn.0.sin(), rn.0.cos());
-
+        let r = Mat2::from_mat3(Mat3::from_quat(t.rotation));
 
         let mut available_thrusters = HashMap::new();
 
@@ -287,7 +354,7 @@ fn thruster_alloc(mut query: Query<(&Object, &Input, &Transform, &CenterOfMass, 
         const MAX_V_MAG: f32 = 50.0;
         const MAX_A: f32 = PI / 2.0;
         const R: f32 = 0.99;
-        const F: f32 = 10000.0;
+        const F: f32 = 1.0;
         const TF: f32 = 1200.0;
 
         if v_mag > MAX_V_MAG * d_mag {
@@ -295,9 +362,8 @@ fn thruster_alloc(mut query: Query<(&Object, &Input, &Transform, &CenterOfMass, 
         }
         if a.0.abs() > MAX_A * i.rotate.abs() {
             a.0 = MAX_A * i.rotate;
-        } else 
-            if v_mag >= 0.0 {
-                a.0 = R * a.0;
+        } else if v_mag >= 0.0 {
+            a.0 = R * a.0;
         }
 
         if i.rotate != 0.0 {
@@ -313,8 +379,6 @@ fn thruster_alloc(mut query: Query<(&Object, &Input, &Transform, &CenterOfMass, 
         if !should_thrust {
             let rev_t = F * -a.0.signum();
             let rev_f = F * -(r.inverse() * v.0);
-
-            let inertial_dampeners_on = false;
 
             if a.0.abs() > EPSILON {
                 desired_torque = rev_t;
@@ -337,15 +401,23 @@ fn thruster_alloc(mut query: Query<(&Object, &Input, &Transform, &CenterOfMass, 
                 for combos in available_thrusters.iter().combinations(k) {
                     let mut v = vec![];
                     for (pos, direction) in combos {
-                        let mut force_scaling = 0.0;
-                        if desired_force != Vec2::ZERO && desired_torque == 0.0 && i.direction != Vec2::ZERO {
+                        let force_scaling;
+                        if desired_force != Vec2::ZERO
+                            && desired_torque == 0.0
+                            && i.direction != Vec2::ZERO
+                        {
                             force_scaling = i.direction.length().clamp(0.0, 1.0);
                         } else {
                             force_scaling = i.rotate.abs().clamp(0.0, 1.0);
                         }
                         let mut force = ExternalForce::default();
-                        force.apply_force_at_point(force_scaling * TF * direction.as_vec(), pos.as_vec2() + 0.5, com.0);
-                        v.push((*pos, force,));
+                        force.apply_force_at_point(
+                            force_scaling * direction.as_vec(),
+                            pos.as_vec2() + 0.5,
+                            com.0,
+                        );
+
+                        v.push((*pos, force));
                     }
                     thrusters_in_combo.push(v);
                 }
@@ -355,34 +427,42 @@ fn thruster_alloc(mut query: Query<(&Object, &Input, &Transform, &CenterOfMass, 
             let mut index = None;
 
             {
-                let mut force = Vec2::ZERO;
-                let mut torque = 0.0f32;
                 for (a, thrusters) in thrusters_in_combo.iter().enumerate() {
+                    let mut force = Vec2::ZERO;
+                    let mut torque = 0.0f32;
                     for thruster in thrusters {
                         force += thruster.1.force();
                         torque += thruster.1.torque();
                     }
-                    weighted_outcomes.insert(a, desired_force.dot(force) + (desired_torque - torque).abs());
+                    if force.length() == 0.0f32 && torque == 0.0f32 {
+                        continue;
+                    }
+                    force = force.normalize_or_zero();
+                    //-1 -1 0
+                    //-1 1 1
+                    //1 - -1
+                    
+                    weighted_outcomes.insert(a, (desired_force.dot(force), (desired_torque - torque).abs(), force));
                 }
-                dbg!(desired_force.dot(force), (desired_torque - torque).abs());
                 let mut lowest_weight = f32::MAX;
-                for (a, weight) in weighted_outcomes {
-                    if weight < lowest_weight {
-                        index = Some(a);
-                        lowest_weight = weight;
+                let mut highest_weight = f32::MIN;
+                for (a, weight) in &weighted_outcomes {
+                    if weight.0 >= highest_weight && weight.1 <= lowest_weight {
+                        index = Some(*a);
+                        highest_weight = highest_weight.max(weight.0);
+                        lowest_weight = lowest_weight.min(weight.1);
                     }
                 }
             }
 
             if let Some(i) = index {
-                let mut force = ExternalForce::default();
+                let mut force = ExternalForce::default().with_persistence(false);
                 for thruster in &thrusters_in_combo[i] {
-                    force.apply_force_at_point(thruster.1.force(), thruster.0.as_vec2() + 0.5, com.0);
-                } 
+                    force.apply_force_at_point(r.inverse() * TF * thruster.1.force(), thruster.0.as_vec2() + 0.5, com.0);
+                }
                 *ef = force;
             }
         }
-
     }
 }
 
@@ -394,5 +474,4 @@ fn main() {
     app.add_systems(Update, thruster_alloc);
     app.add_systems(Update, input);
     app.run();
-
 }
